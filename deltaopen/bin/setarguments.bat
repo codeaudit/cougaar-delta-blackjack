20ea747d08a27ddb04ee26ff5fa3762f66122900
aleung <aleung>
2002-05-01 13:59:45 +0000

    delta->deltaopen

--- deltaopen/bin/setarguments.bat
+++ deltaopen/bin/setarguments.bat
@@ -3,7 +3,7 @@
 set MYDOMAINS=-Dorg.cougaar.domain.glm=org.cougaar.glm.ldm.GLMDomain
 set MYCLASSES=org.cougaar.core.node.Node
 
-set MYPROPERTIES=%MYDOMAINS% -Dorg.cougaar.install.path=%COUGAAR_INSTALL_PATH% -Ddelta.plugin.properties=%COUGAAR_INSTALL_PATH%\delta\data\SamplePluginProperties.ini 
+set MYPROPERTIES=%MYDOMAINS% -Dorg.cougaar.install.path=%COUGAAR_INSTALL_PATH% -Ddelta.plugin.properties=%COUGAAR_INSTALL_PATH%\deltaopen\data\SamplePluginProperties.ini 
 set MYPROPERTIES=%MYPROPERTIES% -Dorg.cougaar.core.cluster.persistence.enable=false -Djdbc.drivers=oracle.jdbc.driver.OracleDriver
 
 set MYMEMORY=-Xms100m -Xmx300m
